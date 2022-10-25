a491a0af734c82a2a80007df0387b5a679ce5965
B20107 <e1b20107@st.oit.ac.jp>
2022-10-25 16:27:00 +0900

    sample5-6-1の実施

--- src/main/java/team9/sse/sse/SseApplication.java
+++ src/main/java/team9/sse/sse/SseApplication.java
@@ -2,12 +2,14 @@ package team9.sse.sse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class SseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SseApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SseApplication.class, args);
+  }
 
 }
